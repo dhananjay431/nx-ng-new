5c847b68da8c72e23596c81c7ba8387abe31d8f0
dbel431 <dbel431@gmail.com>
2026-05-23 17:32:51 +0530

    change shell route

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Route } from '@angular/router';
 import { MainLayoutComponent } from '@ng20/layout';
 import { LoginComponent } from '@ng20/auth';
 import { AuthGuard } from '@ng20/auth';
-import { loadRemoteModule } from '@nx/angular/mf';
 
 export const appRoutes: Route[] = [
   {
@@ -27,17 +26,17 @@ export const appRoutes: Route[] = [
       {
         path: 'customer',
         loadChildren: () =>
-          loadRemoteModule('customer', './Routes').then((m) => m.remoteRoutes),
+          import('customer/Routes').then((m) => m.remoteRoutes),
       },
       {
         path: 'billing',
         loadChildren: () =>
-          loadRemoteModule('billing', './Routes').then((m) => m.remoteRoutes),
+          import('billing/Routes').then((m) => m.remoteRoutes),
       },
       {
         path: 'reports',
         loadChildren: () =>
-          loadRemoteModule('reports', './Routes').then((m) => m.remoteRoutes),
+          import('reports/Routes').then((m) => m.remoteRoutes),
       },
     ],
   },
