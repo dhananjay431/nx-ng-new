c7dba5ed5f2d4fbdbb0498cabbbbf16292099453
dbel431 <dbel431@gmail.com>
2026-05-24 13:31:30 +0530

    run on prod

--- apps/shell/webpack.prod.config.ts
+++ apps/shell/webpack.prod.config.ts
@@ -9,16 +9,11 @@ import config from './module-federation.config';
 export default withModuleFederation(
   {
     ...config,
-    /*
-     * Remote overrides for production.
-     * Each entry is a pair of a unique name and the URL where it is deployed.
-     *
-     * e.g.
-     * remotes: [
-     *   ['app1', 'https://app1.example.com'],
-     *   ['app2', 'https://app2.example.com'],
-     * ]
-     */
+    remotes: [
+      ['customer', 'http://localhost:4201'],
+      ['billing', 'http://localhost:4202'],
+      ['reports', 'http://localhost:4203'],
+    ],
   },
   { dts: false },
 );
