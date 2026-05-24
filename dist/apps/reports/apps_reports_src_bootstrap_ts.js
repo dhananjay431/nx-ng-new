2f904037953e2607fe2c995374f33b4bcdf7b53c
dbel431 <dbel431@gmail.com>
2026-05-24 16:16:30 +0530

    dist add

---
+++ dist/apps/reports/apps_reports_src_bootstrap_ts.js
@@ -0,0 +1,60 @@
+(self["webpackChunkreports"] = self["webpackChunkreports"] || []).push([["apps_reports_src_bootstrap_ts"],{
+
+/***/ 9024
+/*!********************************************!*\
+  !*** ./apps/reports/src/app/app.config.ts ***!
+  \********************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   appConfig: () => (/* binding */ appConfig)
+/* harmony export */ });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ 2699);
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/router */ 3429);
+/* harmony import */ var _app_routes__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app.routes */ 1544);
+
+
+
+const appConfig = {
+  providers: [(0,_angular_core__WEBPACK_IMPORTED_MODULE_0__.provideBrowserGlobalErrorListeners)(), (0,_angular_router__WEBPACK_IMPORTED_MODULE_1__.provideRouter)(_app_routes__WEBPACK_IMPORTED_MODULE_2__.appRoutes)]
+};
+
+/***/ },
+
+/***/ 1544
+/*!********************************************!*\
+  !*** ./apps/reports/src/app/app.routes.ts ***!
+  \********************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   appRoutes: () => (/* binding */ appRoutes)
+/* harmony export */ });
+const appRoutes = [{
+  path: '',
+  loadChildren: () => __webpack_require__.e(/*! import() */ "common").then(__webpack_require__.bind(__webpack_require__, /*! ./remote-entry/entry.routes */ 4409)).then(m => m.remoteRoutes)
+}];
+
+/***/ },
+
+/***/ 1831
+/*!***************************************!*\
+  !*** ./apps/reports/src/bootstrap.ts ***!
+  \***************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ 5442);
+/* harmony import */ var _app_app_config__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./app/app.config */ 9024);
+/* harmony import */ var _app_remote_entry_entry__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/remote-entry/entry */ 8929);
+
+
+
+(0,_angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__.bootstrapApplication)(_app_remote_entry_entry__WEBPACK_IMPORTED_MODULE_2__.RemoteEntry, _app_app_config__WEBPACK_IMPORTED_MODULE_1__.appConfig).catch(err => console.error(err));
+
+/***/ }
+
+}])
+//# sourceMappingURL=apps_reports_src_bootstrap_ts.js.map
