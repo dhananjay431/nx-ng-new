eccbab288363d2f6b40792e0e708007684d8aeb2
dbel431 <dbel431@gmail.com>
2026-05-24 17:44:38 +0530

    new change

--- apps/shell/webpack.prod.config.ts
+++ apps/shell/webpack.prod.config.ts
@@ -10,9 +10,21 @@ export default withModuleFederation(
   {
     ...config,
     remotes: [
-      ['customer', 'http://localhost:4201'],
+      /* ['customer', 'http://localhost:4201'],
       ['billing', 'http://localhost:4202'],
-      ['reports', 'http://localhost:4203'],
+      ['reports', 'http://localhost:4203'], */
+      [
+        'customer',
+        'https://code.dhananjaypro.in/dhananjay431/nx-ng-new/main/dist/apps/customer',
+      ],
+      [
+        'billing',
+        'https://code.dhananjaypro.in/dhananjay431/nx-ng-new/main/dist/apps/billing',
+      ],
+      [
+        'reports',
+        'https://code.dhananjaypro.in/dhananjay431/nx-ng-new/main/dist/apps/reports',
+      ],
     ],
   },
   { dts: false },
